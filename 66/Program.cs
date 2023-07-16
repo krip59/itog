c15ba0cf3907e918071eaf98142943199402bb4b
Irina_Leskova <krip@mail.ru>
2023-07-16 13:48:33 +0500

    Выполнила 2 задания

---
+++ 66/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int degre = Degre(numberA,numberB);
+Console.WriteLine(degre);
+int Degre(int numA, int numB)
+{
+    if (numB == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return numA + Degre(numA, numB-1);
+    }
+
+}
